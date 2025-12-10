ce743c8403bc058a911bb48a9fb3216f3af6dabc
Valerie Richmond <valerie.richmond499@gmail.com>
2025-12-10 15:03:50 -0700

    feat: remove unused block editor file

--- type-3-audio.php
+++ type-3-audio.php
@@ -32,7 +32,6 @@ define('T3A_VERSION', '1.6');
 
 // Include required files
 require_once T3A_PLUGIN_PATH . 'includes/shortcode-player.php';
-require_once T3A_PLUGIN_PATH . 'includes/block-editor.php';
 require_once T3A_PLUGIN_PATH . 'includes/admin-settings.php';
 require_once T3A_PLUGIN_PATH . 'includes/regeneration.php';
 require_once T3A_PLUGIN_PATH . 'includes/manage-narration-metabox.php';
